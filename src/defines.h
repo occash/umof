b0877deb9d106d13a8a93d3c4c372c7973e1299a
occash <artiom.shal@gmail.com>
2015-02-18 14:49:19 +0300

    Improve compiler check

--- src/defines.h
+++ src/defines.h
@@ -22,30 +22,52 @@ USA.
 #ifndef DEFINES_H
 #define DEFINES_H
 
-#if defined(_WIN32) || defined(__CYGWIN__)
-#if defined UMOF_LIBRARY
-#if defined(__GNUC__)
-#define UMOF_EXPORT __attribute__ ((dllexport))
-#elif defined(_MSC_VER)
-#define UMOF_EXPORT __declspec(dllexport)
+#if defined(_WIN32) && !defined(__CYGWIN__)
+#define PLATFORM_WINDOWS
+#elif defined(__APPLE__) && defined(__MACH__)
+#include <TargetConditionals.h>
+#if defined(TARGET_OS_MAC)
+#define PLATFORM_MAC
+#elif defined(TARGET_OS_IPHONE)
+#define PLATFORM_IOS
+#endif
+#elif defined(__linux__)
+#if defined(__ANDROID__)
+#define PLATFORM_ANDROID
+#else
+#define PLATFORM_LINUX
+#endif
+#else
+#error Platform not defined!
+#endif
+
+#if defined(PLATFORM_WINDOWS)
+#if defined(__GNUC__) //GCC on Windows
+#define DECL_EXPORT __attribute__ ((dllexport))
+#define DECL_IMPORT __attribute__ ((dllimport))
+#elif defined(_MSC_VER) //Visual Studio
+#define DECL_EXPORT __declspec(dllexport)
+#define DECL_IMPORT __declspec(dllimport)
+#else //Other compiler
+#define DECL_EXPORT
+#define DECL_IMPORT
+#endif
+#else
+#if __GNUC__ >= 4 //GNU GCC
+#define DECL_EXPORT __attribute__ ((visibility ("default")))
+#define DECL_IMPORT
+#else //Other
+#define DECL_EXPORT
+#define DECL_IMPORT
+#endif
+#endif
+
+#if defined(UMOF_LIBRARY) && !defined(UMOF_STATIC)
+#define UMOF_EXPORT DECL_EXPORT
+#elif !defined(UMOF_STATIC)
+#define UMOF_EXPORT DECL_IMPORT
 #else
 #define UMOF_EXPORT
-#endif
-#else
-#if defined(__GNUC__)
-#define UMOF_EXPORT __attribute__ ((dllimport))
-#elif defined(_MSC_VER)
-#define UMOF_EXPORT __declspec(dllimport)
-#else
-#define  UMOF_EXPORT
-#endif
-#endif
-#else
-#if __GNUC__ >= 4
-#define UMOF_EXPORT __attribute__ ((visibility ("default")))
-#else
-#define UMOF_EXPORT
-#endif
 #endif
 
 class Any;
